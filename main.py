269de2d46d12ba6c893618507179123d58993f0f
Arina <arina2004popova@yandex.ru>
2021-11-05 15:57:46 +0300

    Исправлены опечатки

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fetch_nasa import downloads_images_earth
 from fetch_nasa import downloads_images_from_the_nasa
 from fetch_spacex import fetch_spacex_last_launch
 
-def main():
+if __name__ == '__main__':
     load_dotenv()
     downloads_images_from_the_nasa()
     downloads_images_earth()
@@ -20,7 +20,5 @@ def main():
     while True:
         time.sleep(timer)
         random_path = choice(names_directions)
-        bot.send_document(chat_id=os.getenv('CHAT_ID'), document=open(f'{random_path}/{choice(os.listdir(random_path))}', 'rb'))
-
-if __name__ == '__main__':
-    main()
+        bot.send_document(chat_id=os.getenv('CHAT_ID'),
+                          document=open(f'{random_path}/{choice(os.listdir(random_path))}', 'rb'))
