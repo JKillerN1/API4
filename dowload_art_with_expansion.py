f48364d0739499a2c311bfb242eb771dc6542ae6
Arina <arina2004popova@yandex.ru>
2021-11-05 15:19:32 +0300

    Исправлен README

---
+++ dowload_art_with_expansion.py
@@ -0,0 +1,14 @@
+import os
+from urllib.parse import urlparse
+
+import requests
+
+def expansion (url):
+    image_expansion = os.path.splitext(urlparse(url).path)
+    return image_expansion[1]
+
+def dowloands_art (url, filename):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filename, 'wb') as file:
+        file.write(response.content)
