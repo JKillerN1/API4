11fadcc8a0878057a138486c73cd6e4c038891ca
Arina <arina2004popova@yandex.ru>
2021-11-17 15:55:56 +0300

    Алгоритм публикации вынесен в отдельную функцию

--- main.py
+++ main.py
@@ -10,21 +10,25 @@ from fetch_nasa import download_earth_images
 from fetch_nasa import download_nasa_images
 from fetch_spacex import fetch_spacex_last_launch
 
+def sending_in_telegram_image(names_directions, bot):
+    random_path = choice(names_directions)
+    if len(os.listdir(random_path)) != 0:
+        bot.send_photo(chat_id=os.getenv('CHAT_ID'),
+                       photo=open(f'{random_path}/{choice(os.listdir(random_path))}', 'rb'))
+
+
 if __name__ == '__main__':
-    Path("images").mkdir(parents=True, exist_ok=True)
-    Path("images_earth").mkdir(parents=True, exist_ok=True)
-    Path("images_nasa").mkdir(parents=True, exist_ok=True)
     load_dotenv()
     api_key = os.getenv('API_KEY')
+    names_directions = ['images_earth', 'images', 'images_nasa']
+    Path(names_directions[1]).mkdir(parents=True, exist_ok=True)
+    Path(names_directions[0]).mkdir(parents=True, exist_ok=True)
+    Path(names_directions[2]).mkdir(parents=True, exist_ok=True)
     download_nasa_images(api_key)
     download_earth_images(api_key)
     fetch_spacex_last_launch()
     bot = telegram.Bot(token=os.getenv('TOKEN_BOT'))
-    names_directions = ['images_earth', 'images', 'images_nasa']
     timer = 86400
     while True:
-        random_path = choice(names_directions)
-        if len(os.listdir(random_path)) != 0:
-            bot.send_photo(chat_id=os.getenv('CHAT_ID'),
-                              photo=open(f'{random_path}/{choice(os.listdir(random_path))}', 'rb'))
-            time.sleep(timer)
+        sending_in_telegram_image(names_directions, bot)
+        time.sleep(timer)
