a4891076812af8917d0ed9d80d080c2d296dda21
Arina <arina2004popova@yandex.ru>
2021-11-24 15:44:56 +0300

    Функции изолированы от внешнего окружения

--- main.py
+++ main.py
@@ -10,19 +10,20 @@ from fetch_nasa import download_earth_images
 from fetch_nasa import download_nasa_images
 from fetch_spacex import fetch_spacex_last_launch
 
-def send_image_in_telegram(folder_names, bot):
+def send_image_in_telegram(folder_names, bot, chat_id):
     random_path = choice(folder_names)
     files_in_folder = os.listdir(random_path)
     if files_in_folder:
         with open(f'{random_path}/{choice(files_in_folder)}', 'rb') as image:
             art = image.read()
-            bot.send_photo(chat_id=os.getenv('CHAT_ID'),
+            bot.send_photo(chat_id=chat_id,
                         photo=art)
 
 
 if __name__ == '__main__':
     load_dotenv()
     api_key = os.getenv('API_NASA_KEY')
+    chat_id = os.getenv('CHAT_ID')
     folder_names = ['images_earth', 'images', 'images_nasa']
     for image in folder_names:
         Path(image).mkdir(parents=True, exist_ok=True)
